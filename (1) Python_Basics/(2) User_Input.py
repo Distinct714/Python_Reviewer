eeb11458c2ff459b1535712da534a6c60bf7acdd
Distinct <distinct3579@gmail.com>
2025-05-15 19:59:29 +0800

    Add files via upload

---
+++ (1) Python_Basics/(2) User_Input.py
@@ -0,0 +1,59 @@
+
+# GETTING INPUT FROM USER
+
+# The input function ask or allow the users to type or input their data or information into the program.
+# Input function automatically convert into string. 
+# If you want to modify the variables into other data types, you can convert into integer, or float. This is called casting.
+
+# SAMPLE ONE
+
+name = input("Enter your Name: ")
+print(f"Hello, {name}!")
+
+age = int(input("Enter Your Age: "))
+asking = input(f"Are you {age} years old?")
+
+if age >= 18:
+    if asking == "Yes" or "yes":
+        print("Then, get some work!\n")
+    else:
+        print("Then, study now!\n")
+else:
+    print("Repeat the fill-up.\n")
+
+# SAMPLE TWO
+
+prompt = "Your data won\'t be stolen.\n"
+prompt += "Enter your username: "
+username = input(prompt)
+print(f"Hello, {username}.")
+
+
+# (F-STRING) Formatting String
+
+# F-string      This is a preferred way of formatting strings and allows you to format selected parts of a string.
+
+#               Note, we cannot combine strings and numbers in a variables. Instead, use f-string and {variable} like this:
+
+name = "Joshua" # String
+age = 19        # Integer
+
+print(f"My name is {name}")
+print(f"I am {age} years old")
+
+# Inside the curly brackets {} is a placeholders for variables and other operations to format the value.
+
+
+# Placeholder   This can contain variables, operations, functions, and modifiers to format the value.
+#               It can be identified using named indexes {variable}, numbered indexes {0}, or even empty placeholders {}.
+
+# Modifier      This is included by adding a colon : followed by a legal formatting type, like .2f, .3f and so on
+#               which means fixed point number with 2 or more decimals like this:
+
+name = "Joshua" # String
+age = 19        # Integer
+
+print(f"My name is {name}")
+print(f"I am {age:.2f} years old")
+
+# Explore more about formatting types in string_format.
---
+++ (1) Python_Basics/(4) Escape_Characters.py
@@ -0,0 +1,64 @@
+
+# ESCAPE CHARACTERS
+
+# To insert characters that are illegal in a string, use an escape character.
+# An escape character is a backslash \ followed by the character you want to insert.
+
+# Whitespace is a non-printing characters and its include spaces, tabs, newlines and other characters that create space in text.
+
+# You can also use double backslash \\ to insert one backslash.
+
+phrase1 = "I am Unknown\\Guy."
+print(phrase1)
+
+
+# \n function is a special character that is used when we need to end the current line and start a new line.
+# It is used to create a new line in the string or file. (Enter key in code)
+
+phrase2 = "I am\nUnknown Guy."
+print(phrase2)
+
+
+# \' or \" function is a special character used to insert single or double quotation in a string value.
+
+quotes = "It\'s BOTORU."
+print(quotes) 
+
+phrase3 = "I am \"Unknown\" Guy."
+print(phrase3)
+
+
+# \t function is a special character that allows inserting a space or tab between two or more words. (Indent)
+
+phrase4 = "It was\tBOTORU."
+print(phrase4)
+
+
+# \b function is a special character that represents the backspace character. 
+
+phrase5 = 'Hey \bBOTORU.'
+print(phrase5)
+
+
+# \r    Carriage Return is a special character that is used to move the cursor to the beginning of the line, 
+#       effectively "returning" it to the start.
+
+phrase6 = "Do some\rwork!"
+print(phrase6)
+
+
+# \f    Form Feed is a special character in the ASCII character set that has a few specific uses.
+
+phrase7 = "Do some work!\fADVICE:"
+print(phrase7)
+
+
+# \ooo  Octal value: A backslash followed by three integers will result in a octal value:
+
+txt = "\110\145\154\154\157"
+print(txt) 
+
+
+# \xhh  Hex value: A backslash followed by an 'x' and a hex number represents a hex value:
+txt = "\x48\x65\x6c\x6c\x6f"
+print(txt) 
