70300b27780219c1c4c59489f82e589f333426d5
Distinct <distinct3579@gmail.com>
2025-07-03 09:10:43 +0800

    Update (3) Numbers.py

--- (1) Python_Basics/(3) Numbers.py
+++ (1) Python_Basics/(3) Numbers.py
@@ -1,4 +1,3 @@
-
 # USE OF NUMBERS ( PYTHON MATH )
 
 # Numerical data is a kind of information that comes in the form of numbers.
@@ -10,28 +9,23 @@ print((5 + 4) - 3 * 2 / 1)
 my_number = 5
 print(str(my_number))
 
-
 # The sum() function add all the elements presented in a list or variables.
 num = [1, 2, 3, 4, 5]
 total = sum(num)
 print(total)
-
 
 # The abs() or absolute value return the value of any given or assigned number.
 # Any arithmetic operators such as negative is eliminated here.
 my_number2 = -54
 print(abs(my_number2))
 
-
 # The pow() function consist of a number and power given. It calculate the power of a given number.
 # The first element is given number and second element is power.
 print(pow(3, 2))
 
-
 # The min() and max() function determine the lowest and highest value of a given numbers.
 print(min(54, 78))
 print(max(54, 78))
-
 
 # The round() function is use to round off a given numbers or decimals.
 print(round(49.6))
