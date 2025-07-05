17ec4cc741f84251f43ba452d22ad1ea724e1fcc
Distinct <distinct3579@gmail.com>
2025-07-05 10:59:06 +0800

    Update (0) Operands-Based.py

--- (2) Python_Operators/(0) Operands-Based.py
+++ (2) Python_Operators/(0) Operands-Based.py
@@ -1,4 +1,3 @@
-
 # OPERANDS-BASED
 
 # Operators in Python are symbols used for computation.
@@ -16,28 +15,22 @@ a = -10
 b = +5 
 
 # Unary plus (+). The unary plus doesn't change the value of 'c'
-
 c = -5
 print("Unary Plus:", +c)  
 
 # Unary minus (-). The unary minus negates the value of 'c'
-
 print("Unary Minus:", -c)  
 
 # Unary logical NOT (!). Logical NOT of False is True
-
 d = False
 print("Unary Logical NOT:", not d)  
 
 # Unary bitwise NOT (~). Bitwise NOT of 1 is -2
-
 e = 1
 print("Unary Bitwise NOT:", ~e)
-
 
 # Binary Operator is an operation that contains two operand.
 
 # SAMPLE
 f = 10 + 5 
 print(f)
-
