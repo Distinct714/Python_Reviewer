a517afa96499c792483fd298663a2a8d8457cf88
Distinct <distinct3579@gmail.com>
2025-07-06 09:19:43 +0800

    Update Comparison.py

--- (2) Python_Operators/Comparison.py
+++ (2) Python_Operators/Comparison.py
@@ -1,11 +1,9 @@
-
 # COMPARISON OR RELATIONAL OPERATORS
 
 # <, >, <=, >= and == are comparison operators use in comparing variables.
 # != is another operator for not equal.
 
 # IF-ELIF-ELSE STATEMENTS FUNCTION
-
 def max_num(num1, num2, num3):
     if num1 >= num2 and num1 >=num3:
         return num1
@@ -16,7 +14,7 @@ def max_num(num1, num2, num3):
 
 print(max_num(5,7,9))
 
-
+# USING TYPE ANNOTATIONS
 num: int = 10
 
 if num >= 5:
@@ -24,12 +22,10 @@ if num >= 5:
 else:
     print("No")
 
-
 # TERNARY OPERATOR (Conditional Expressions)
 
 # The ternary operator in Python is simply a shorter way of writing an if and if...else statements.
 # true_value if condition - else false_value
-
 def max_nums(nums1, nums2):
     return nums1 if nums1 >= nums2 else nums2
 
