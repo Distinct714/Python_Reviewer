50dcf70f4608bda58b852f4dcab4c219c06c5899
Distinct <distinct3579@gmail.com>
2025-07-06 09:20:07 +0800

    Update Identity.py

--- (2) Python_Operators/Identity.py
+++ (2) Python_Operators/Identity.py
@@ -1,4 +1,3 @@
-
 # IDENTITY OPERATORS
 
 # These operators compare the memory addresses of objects.
@@ -8,14 +7,11 @@
 
 # "is not" operator returns True if both variables are not equal or same object.
 
-
 x = ["Joshua", "Sean"]
 y = ["Joshua", "Sean"]
 z = x
 
 # Even though x and y variable store the same elements or items, the memory address of objects is different and will return False.
 print(x is y)
-
 print(x is z)
 print(y is not z)
-
