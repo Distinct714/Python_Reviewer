e3de420e7274ccc10db823ad5f4ad61830794492
Distinct <distinct3579@gmail.com>
2025-07-07 13:40:40 +0800

    Update Bitwise.py

--- (2) Python_Operators/Bitwise.py
+++ (2) Python_Operators/Bitwise.py
@@ -1,5 +1,4 @@
 # BITWISE OPERATORS
 
-
 # Bitwise Operators operate on individual bits of data. 
 # They are less frequently used but are important for low-level programming or tasks involving bit manipulation.
