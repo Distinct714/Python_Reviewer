b5f4acf4a981aa0f1d59a879064731819d0f30eb
Distinct <distinct3579@gmail.com>
2025-07-11 09:51:02 +0800

    Update (7) NONE_TYPES.py

--- (3) Built-in_Data_Types/(7) NONE_TYPES.py
+++ (3) Built-in_Data_Types/(7) NONE_TYPES.py
@@ -1,11 +1,6 @@
-
 # NONE TYPE
 
 # The none is used to define a null value or object in Python. 
 # It is not the same as an empty string, a False, or a zero but return none only.
-
 name3 = None
 print(name3)
-
-
-
