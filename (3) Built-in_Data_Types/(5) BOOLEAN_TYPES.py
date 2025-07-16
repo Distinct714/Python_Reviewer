64e034a783fa52fe65b5107a3af9efb806746de0
Distinct <distinct3579@gmail.com>
2025-07-16 20:11:59 +0800

    Update (5) BOOLEAN_TYPES.py

--- (3) Built-in_Data_Types/(5) BOOLEAN_TYPES.py
+++ (3) Built-in_Data_Types/(5) BOOLEAN_TYPES.py
@@ -8,20 +8,16 @@
 # Any list, tuple, set, and dictionary present are also True.
 # In other words, every empty values, such as (), [], {}, "", the number 0, and the value None result False.
 
-
 # The bool() function allows you to identify any value, and give you True or False in return.
 comparison_num = 5 < 4
 print(bool(comparison_num))
 
-# Another Example
+# Another Sample
 a = True
 b = False
 print(bool(a and b))
 
 # Python also has many built-in functions that return a boolean value, like the isinstance() function, 
 # which can be used to determine if an object is of a certain data type. (variable, data types)
-
 x = 200
 print(isinstance(x, int))
-
-
