bc1d8751f36fb8d51fc0300302769c49710835cd
Distinct <distinct3579@gmail.com>
2025-07-19 10:49:29 +0800

    Update Recursion.py

--- (5) Functional_Programming/Recursion.py
+++ (5) Functional_Programming/Recursion.py
@@ -1,4 +1,4 @@
-
+# RECURSION IN PYTHON
 # Recursion is a common mathematical and programming concept. It means that a function calls itself. 
 # This has the benefit of meaning that you can loop through data to reach a result.
 
