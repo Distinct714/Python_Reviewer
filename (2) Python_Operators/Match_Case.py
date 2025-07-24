a48a7707e808322020a7cc2997a3895893219ee5
Distinct <distinct3579@gmail.com>
2025-07-24 11:04:04 +0800

    Update Match_Case.py

--- (2) Python_Operators/Match_Case.py
+++ (2) Python_Operators/Match_Case.py
@@ -14,6 +14,20 @@ def check_number(x):
 check_number(10)
 check_number(30)
 
+# USING IF STATEMENT IN MATCH CASE STATEMENT
+def num_check(x):
+    match x:
+        case 10 if x % 2 == 0:  # Match 10 only if it's even
+            print("Matched 10 and it's even!")
+        case 10:
+            print("Matched 10, but it's not even.")
+        case _:
+            print("No match found")
+
+num_check(10)
+num_check(15)
+
+
 # MAKING A CALCULATOR USING MATCH CASE STATEMENT WITH TRY-EXCEPT 
 while True:
     try:
